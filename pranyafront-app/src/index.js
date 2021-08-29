efd1582c57fe92ff544dae78936ebe62df14bf5c
achint740 <achintdawra740@gmail.com>
2021-08-29 12:14:12 +0530

    Firebase Authentication Setup

--- pranyafront-app/src/index.js
+++ pranyafront-app/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App'
+import {initializeApp} from 'firebase/app';
+import {firebaseConfig} from './fireAuth/cred'
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />,document.getElementById('root'));
